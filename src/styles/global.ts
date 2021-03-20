aaaab10b21e30fdbf129f926c854cd61d9c7189e
André Santiago <andresantiagohellmann@gmail.com>
2021-03-20 16:45:02 -0300

    feat: add custom fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,10 +4,59 @@ import { normalize } from 'styled-normalize'
 const GlobalStyles = createGlobalStyle`
   ${normalize}
 
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-regular.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: italic;
+    font-weight: 500;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-500italic.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-600.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-700.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 800;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-800.woff2') format('woff2')
+  }
+
   * {
     margin: 0;
     padding: 0;
+
     box-sizing: border-box;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html{
@@ -19,7 +68,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body{
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Montserrat', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
     font-size: 1rem;
   }
